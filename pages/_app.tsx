f8b58b03e126ee62bf97d415ee129ef0c69b234b
ytakahashi2020 <y.takahashi2020@gmail.com>
2023-06-18 07:46:40 +0900

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+import type { AppProps } from "next/app";
+import { ThirdwebProvider, localWallet, metamaskWallet, smartWallet } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+import { Astar } from "@thirdweb-dev/chains";
+
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+// const activeChain = "mumbai";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider 
+      activeChain={Astar}
+      supportedWallets={[
+        metamaskWallet(),
+        smartWallet({
+          factoryAddress: process.env.NEXT_PUBLIC_FACTORY_ADDRESS!,
+          thirdwebApiKey: process.env.NEXT_PUBLIC_API_KEY!,
+          gasless: true,
+          personalWallets:[
+            metamaskWallet()
+          ]
+        })
+      ]}
+      >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
